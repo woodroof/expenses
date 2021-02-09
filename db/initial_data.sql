82ea4ce9825d458794aefce6e385febd723fa0e2
Ильиных Данил Сергеевич <d.ilinyh@2gis.ru>
2021-02-09 23:11:46 +0700

    Added some events to initial db data

--- db/initial_data.sql
+++ db/initial_data.sql
@@ -24,6 +24,13 @@ values
   ('delete', '^/users/[^/]+/?$', 'delete_user', true),
   ('delete', '^/expenses/[^/]+/[^/]+/?$', 'delete_user_expense', true);
 
+select handlers.put_user_expense(1, '/expenses/admin/1', '{"date": "2021-01-01 12:00:00", "description": "nearby shop", "amount": 1, "comment": "recreation"}');
+select handlers.put_user_expense(2, '/expenses/user_manager/1', '{"date": "2021-01-02 12:00:00", "description": "steam", "amount": 2, "comment": "cyberpunk"}');
+select handlers.put_user_expense(3, '/expenses/user/1', '{"date": "2021-01-03 12:00:00", "description": "online shop", "amount": 3, "comment": "clothes"}');
+select handlers.put_user_expense(4, '/expenses/extended_user/1', '{"date": "2021-01-04 12:00:00", "description": "gas station", "amount": 4, "comment": "gas & chocolate"}');
+select handlers.put_user_expense(4, '/expenses/extended_user/2', '{"date": "2021-01-04 13:00:00", "description": "gas station", "amount": 2, "comment": "more chocolate"}');
+select handlers.put_user_expense(4, '/expenses/extended_user/3', '{"date": "2021-01-04 14:00:00", "description": "gas station", "amount": 2, "comment": "even more chocolate"}');
+
 drop role if exists http;
 create role http noinherit login password 'http';
 grant usage on schema api to http;
